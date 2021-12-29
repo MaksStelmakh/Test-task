466c8dd4a53f3c28f305af07a239daa257b74e1c
MaksStelmakh <84736957+MaksStelmakh@users.noreply.github.com>
2021-12-29 18:10:57 +0000

    Deploying to gh-pages from @ MaksStelmakh/Test-task@ce290d671fd1f1a94f8d7c8f78e162773f2482fd 🚀

--- src.1a6a992c.js
+++ src.1a6a992c.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/project-sprite.svg":[["project-sprite.d4880f0d.svg","Audr"],"Audr"],"./../images/main/main.jpg":[["main.7b450135.jpg","Jcc1"],"Jcc1"],"./../images/main/main-mobile.jpg":[["main-mobile.b13c9934.jpg","TBqr"],"TBqr"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.6172382e.js.map
+//# sourceMappingURL=/parcel-project-template/src.1a6a992c.js.map
