3204be7f8cfbe4c12890623e3837e0ac3da38b33
MaksStelmakh <step4ick2017@gmail.com>
2021-12-29 17:11:11 +0200

    Доделал секцию Team

---
+++ src/js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.multiple-items').slick({
+    infinite: true,
+    slidesToScroll: 1,
+    arrows: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
